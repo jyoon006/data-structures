48d34c88adabcd271ec9afaf355f4eec1187edae
Jisoo Yoon <jyoon006@gmail.com>
2016-01-13 16:07:16 -0500

    inital commit

---
+++ sprint-one/src/functional/queue.js
@@ -0,0 +1,27 @@
+var Queue = function(){
+  var someInstance = {};
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var count = 0;
+  // Implement the methods below
+
+  someInstance.enqueue = function(value){
+    count++;
+    storage[count] = value;
+  };
+
+  someInstance.dequeue = function(){
+    var x = storage[count];
+    delete storage[count];
+    if (count > 0) {
+      count--;
+    }
+    return x;
+  };
+
+  someInstance.size = function(){
+    return count;
+  };
+  //testing
+  return someInstance;
+};
