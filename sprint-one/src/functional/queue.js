28416ec02602b4ecff98df9271e4bc81f8c70cdc
Jisoo Yoon <jyoon006@gmail.com>
2016-01-13 16:17:33 -0500

    changes to commit/ removed unecessary duplicated code

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -28,20 +28,5 @@ var Queue = function(){
 };
 
 
-  // Use an object with numeric keys to store values
-  var storage = {};
 
-  // Implement the methods below
-
-  someInstance.enqueue = function(value){
-  };
-
-  someInstance.dequeue = function(){
-  };
-
-  someInstance.size = function(){
-  };
-
-  return someInstance;
-};
 
