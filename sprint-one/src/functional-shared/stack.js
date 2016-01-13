6864c1f11645c9c7e89645c9a63c74455f374f3e
Jisoo Yoon <jyoon006@gmail.com>
2016-01-13 18:41:22 -0500

    init commit and passed first test for 0

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var stack = {storage: {}, size: 0};
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods =  {
+  size: function() {  
+  return 0;
+  }
+};
 
-
+//Stack.size()
