3bc17f61aab7095145a7f0db926058cec8f4bd0b
Jisoo Yoon <jyoon006@gmail.com>
2016-01-14 14:22:58 -0500

    passed test for push method pseudo/stack.js

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,12 +1,18 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
 
 Stack.prototype.size = function() {
-  return 0;
+  return this.count;
 };
+
+Stack.prototype.push = function() {
+  this.count++;
+}
+
 
 
 
