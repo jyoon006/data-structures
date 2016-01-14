35a089b6a8a3c7f610e51dee46c8335b03ebe7a2
Jisoo Yoon <jyoon006@gmail.com>
2016-01-14 14:06:07 -0500

    passed 2nd test for pseudo/queue.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,8 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.list = {};
 };
 
 Queue.prototype.size = function () {
-  return 0;
+  return this.count;
+}
+
+Queue.prototype.enqueue = function (value) {
+  this.count++;
 }
