d25f40f0107fefbc1095f3a294d71b4f2b404eb3
Jisoo Yoon <jyoon006@gmail.com>
2016-01-14 14:13:16 -0500

    passed all test for psedu/queue.js

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,6 +3,8 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   this.count = 0;
   this.list = {};
+  this.begin = 1;
+  this.end = 1;
 };
 
 Queue.prototype.size = function () {
@@ -11,10 +13,16 @@ Queue.prototype.size = function () {
 
 Queue.prototype.enqueue = function (value) {
   this.count++;
+  this.list[this.end] = value;
+  this.end++;
 }
 
 Queue.prototype.dequeue = function () {
+  var temp = this.list[this.begin];
+  delete this.list[this.begin];
+  this.begin++;
   if (this.count > 0) {
     this.count--;
   }
+  return temp;g
 }
