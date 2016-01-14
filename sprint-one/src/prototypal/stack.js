cfdbd41f09702c136f28bf67270d29a2c55d500b
Jisoo Yoon <jyoon006@gmail.com>
2016-01-13 22:57:41 -0500

    initial commit/first test passed for prototypal/stack.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var x = Object.create(stackMethods);
+  x.counter = 0;
+  x.list = {};
+  return x;
 };
 
-var stackMethods = {};
+
+//object.create(stackMethods)
+
+var stackMethods = {
+    push: function() {
+
+    },
+    pop: function() {
+  
+    },
+    size: function() {
+      return 0;
+    }      
+  
+
+};
+
 
 
