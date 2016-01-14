cef2f2d994bfb08be7559cbf741d3e84e8e4b093
Jisoo Yoon <jyoon006@gmail.com>
2016-01-14 13:40:50 -0500

    passed first test for prototypal/queue.js

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,15 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var x = Object.create(queueMethods);
+  return x;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {   
+    return 0;
+  }
+};
+
 
 
