c80df63058f216dd3e8504e42c08f8959c8ec17a
Jisoo Yoon <jyoon006@gmail.com>
2016-01-15 22:41:12 -0500

    updated binarySearchTreeSpec - each before test, we created new instance of binarysearchtree class/ passed first test for binarysearchtree to pass that insert contains and depthfirstlog is a method of binaryserachtree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,20 @@
-var BinarySearchTree = function(value){
+var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left;
+  this.right;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  
+}
 
+BinarySearchTree.prototype.contains = function(value){
+  
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+  
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
