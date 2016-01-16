7d14bcb371f0c8112f88946aa38cbbbfbf6c61b1
Jisoo Yoon <jyoon006@gmail.com>
2016-01-16 13:10:53 -0500

    updated hashtablehelper for getIndexBelowMaxForKey so that no matter what the typeof str is passed in as argument, it will return as a string putting if statement for JSON.stringify

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,13 +1,13 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = {};
+  set._storage = new HashTable();
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
-  this._storage[item] = item;
+  this._storage.insert(JSON.stringify(item), item);
 };
 
 setPrototype.contains = function(item){
@@ -21,8 +21,8 @@ setPrototype.remove = function(item){
 /*
  * Complexity: What is the time complexity of the above functions?
  *
- * add()    : O(1)
- * contain(): O(1)
- * remove() : O(1)
+ * add()     : O(1)
+ * contain() : O(1)
+ * remove()  : O(1)
  *
  */
