ca5d9072036c03bf5098229a1e1ea0e44ffaaf77
Jisoo Yoon <jyoon006@gmail.com>
2016-01-16 17:11:56 -0500

    changed hashtablehelpers for storage array back to original from testing out the storage array for test cases

--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -41,9 +41,6 @@ var LimitedArray = function(limit){
 // to turn any string into an integer that is well-distributed between the
 // numbers 0 and `max`
 var getIndexBelowMaxForKey = function(str, max){
-  if (typeof str !== 'string') {
-    str = JSON.stringify(str);
-  }
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
     hash = (hash<<5) + hash + str.charCodeAt(i);
