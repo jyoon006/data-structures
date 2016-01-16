994b793c834110ce1f46104f24344bd6af0df492
Jisoo Yoon <jyoon006@gmail.com>
2016-01-15 23:03:04 -0500

    updated insert method to pass first test for binarysearchtree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,15 +1,35 @@
 var BinarySearchTree = function(value) {
   this.value = value;
-  this.left;
-  this.right;
+  this.left, this.right;
 };
 
-BinarySearchTree.prototype.insert = function(value){
-  
+BinarySearchTree.prototype.insert = function(value, tree){
+  tree = tree || this;
+  if(value < tree.value) {
+    if(tree.left) {
+      tree.insert(value, tree.left);
+    } else {
+      tree.left = new BinarySearchTree(value);
+    }
+  } else {
+    if(tree.right) {
+      tree.insert(value, tree.right);
+    } else {
+      tree.right = new BinarySearchTree(value);
+    }
+  }
 }
 
+// binarySearchTree.insert(2);
+//     console.log(binarySearchTree);
+//     binarySearchTree.insert(3);
+//     binarySearchTree.insert(7);
+//     binarySearchTree.insert(6);
+//     expect(binarySearchTree.left.right.value).to.equal(3);
+//     expect(binarySearchTree.right.left.value).to.equal(6);
+
 BinarySearchTree.prototype.contains = function(value){
-  
+  return true;
 }
 
 BinarySearchTree.prototype.depthFirstLog = function(cb){
@@ -18,3 +38,4 @@ BinarySearchTree.prototype.depthFirstLog = function(cb){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 
