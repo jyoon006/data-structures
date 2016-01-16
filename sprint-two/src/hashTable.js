42e5c32298330ddb109e03803a9cefc58e214157
Jisoo Yoon <jyoon006@gmail.com>
2016-01-15 21:55:20 -0500

    updated retrieve and insert method to pass collisions test and every other not requiring remove method for hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,25 +5,40 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(key, value){
   var i = getIndexBelowMaxForKey(key, this._limit);
-  this._storage.set(i, [key, value]);
+  console.log(this.retrieve(i));
+  if (this._storage.get(i)) {  
+    for(var j = 0; j < this._storage.get(i).length; j++) {
+      if(this._storage.get(i)[j][0] === key) {
+        this._storage.get(i)[j][1] = value;
+        return true;
+      } 
+    }
+    this._storage.get(i).push([key, value]); 
+    return true;   
+  }
+  this._storage.set(i, [[key, value]]);
+  return true;
 };
 
 HashTable.prototype.retrieve = function(key){
   var i = getIndexBelowMaxForKey(key, this._limit);
-  // return this._storage.get(i)[1];
-  if(this._storage.get(i)) {   
-    return this._storage.get(i)[1];
-  }
-  else {
+  if(this._storage.get(i)) {  
+    for(var j = 0; j < this._storage.get(i).length; j++) {
+      if(this._storage.get(i)[j][0] === key) {
+        return this._storage.get(i)[j][1];
+        
+      } 
+    }
     return null;
   }
+  return null;
 };
 
 HashTable.prototype.remove = function(key){
   var value = this.retrieve(key);
   this._storage.each(function (item, index, array) {
     if(item) {
-      console.log(item[1], array[index]);
+      // console.log(item[1], array[index]);
       if(item[1] == value ) {
         array[index] = null;
       }
