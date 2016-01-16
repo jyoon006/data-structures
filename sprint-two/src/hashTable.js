eb26004cd918939f076bc6a7020500f61f9752f0
Jisoo Yoon <jyoon006@gmail.com>
2016-01-16 17:11:19 -0500

    deleted x in hashtablespec for _resize method to run the test/ passed test _resize method for hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,7 @@
-var HashTable = function(){
-  this._limit = 8;
+var HashTable = function(limit){
+  this._limit = limit || 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(key, value){
@@ -13,9 +14,17 @@ HashTable.prototype.insert = function(key, value){
       } 
     }
     this._storage.get(i).push([key, value]); 
+    this._count++;
+      if(this._count >= this._limit * .75) {
+        this._resize(this._limit * 2);
+      }
     return true;   
   }
   this._storage.set(i, [[key, value]]);
+  this._count++;
+    if(this._count >= this._limit * .75) {
+      this._resize(this._limit * 2);
+    }
   return true;
 };
 
@@ -25,7 +34,6 @@ HashTable.prototype.retrieve = function(key){
     for(var j = 0; j < this._storage.get(i).length; j++) {
       if(this._storage.get(i)[j][0] === key) {
         return this._storage.get(i)[j][1];
-        
       } 
     }
     return null;
@@ -34,12 +42,18 @@ HashTable.prototype.retrieve = function(key){
 };
 
 HashTable.prototype.remove = function(key){
+  var context = this;
   this._storage.each(function (item, index, array) {
     if(item) {
       for (var j = 0; j < item.length; j++) {
         if(item[j]) {
           if (item[j][0] === key) {
+            context._count--;
+            console.log('decreased');
             item.splice(j, 1);
+            if(context._count < context._limit * .25) {
+              context._resize(context._limit/2);
+            }
             return true;
           }
         }
@@ -49,7 +63,27 @@ HashTable.prototype.remove = function(key){
   });
 };
 
+HashTable.prototype._resize = function(max){
+  
+  var tempStorage = new LimitedArray(this._limit);
+  this._storage.each(function(item, index, array) {
+    tempStorage.set(index, item);    
+  });
+  
+  this._limit = max;
+  this._count = 0;
+  this._storage = new LimitedArray(this._limit);
 
+  var context = this;
+  tempStorage.each(function(item, index, array) {
+    if (item) {
+      for (var i = 0; i < item.length; i++){
+        context.insert(item[i][0], item[i][1]);
+      }
+    }
+  });
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
